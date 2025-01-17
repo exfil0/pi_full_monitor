40f7e35375b0ef84da3e7b0b8cb94736105ad5c6
Karim Jaber (Exfil0) <84948741+exfil0@users.noreply.github.com>
2025-01-17 22:25:29 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
 echo "Setting up pi_full_monitor..."
-
-# Create a virtual environment
 python3 -m venv venv
 source venv/bin/activate
-
-# Upgrade pip
 pip install --upgrade pip
-
-# Install the package
 pip install .
-
-echo "pi_full_monitor is now installed. Run it with 'pi_full_monitor'."
+echo "Installation complete. You can now run 'pi_full_monitor'."
