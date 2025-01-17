4b254dcb10cd8751788479f136ae69efe333bc0d
Karim Jaber (Exfil0) <84948741+exfil0@users.noreply.github.com>
2025-01-17 22:10:53 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,18 +2,14 @@
 
 echo "Setting up pi_full_monitor..."
 
-# Check for and install Python dependencies
-echo "Installing Python dependencies..."
-sudo apt update && sudo apt install -y python3 python3-venv python3-pip
-
 # Create a virtual environment
-echo "Creating a virtual environment..."
 python3 -m venv venv
 source venv/bin/activate
 
+# Upgrade pip
+pip install --upgrade pip
+
 # Install the package
-echo "Installing the pi_full_monitor package..."
 pip install .
 
-# Create a CLI command
 echo "pi_full_monitor is now installed. Run it with 'pi_full_monitor'."
