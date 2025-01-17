1f8f65a756de361f8041bd86c4ab36ccb8104b6a
Karim Jaber (Exfil0) <84948741+exfil0@users.noreply.github.com>
2025-01-17 22:24:32 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,20 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="pi-full-monitor",
+    name="pi_full_monitor",
     version="1.0.0",
+    description="A Raspberry Pi performance monitoring CLI tool",
+    author="Your Name",
+    author_email="your_email@example.com",
     packages=find_packages(),
-    install_requires=[
-        "psutil",
-        "rich",
-    ],
     entry_points={
-        "console_scripts": [
-            "pi_full_monitor=pi_full_monitor.cli:main",  # Adjust path to your main script
-        ]
+        'console_scripts': [
+            'pi_full_monitor=pi_monitor.monitor:main',
+        ],
     },
+    install_requires=[
+        "psutil>=6.0.0",
+        "rich>=13.0.0"
+    ],
+    python_requires=">=3.7",
 )
